717adc7ab23fde6131469725af2081c4df8ac8ee
Arthur <chupryna.2004@mail.ru>
2025-03-16 01:19:39 +0500

    add method generateNumber & redefined the method

--- src/UsualPlayer.java
+++ src/UsualPlayer.java
@@ -1,3 +1,10 @@
+import java.util.Random;
+
 public class UsualPlayer extends Player {
 
+    @Override
+    public void generateNumber() {
+        Random random = new Random();
+        this.number = random.nextInt(60);
+    }
 }
