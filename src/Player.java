581e10ea5187f5b14e0e843c59dd03be8c2c6214
Arthur <chupryna.2004@mail.ru>
2025-03-16 01:13:38 +0500

    add abstract class player & abstract method

---
+++ src/Player.java
@@ -0,0 +1,13 @@
+public abstract class Player {
+
+//    private int position = 0;
+    protected int number;
+
+
+    public int getNumber() {
+        return number;
+    }
+
+    public abstract void generateNumber();
+
+}
