ff7e8ac3fc1d866806a35c312958ae2fe709560c
Arthur <chupryna.2004@mail.ru>
2025-03-16 01:20:25 +0500

    add calling the method

--- src/Program.java
+++ src/Program.java
@@ -2,5 +2,20 @@ public class Program {
 
     public static void main(String[] args) {
 
+        Player usualPlayer = new UsualPlayer();
+        Player premiumPlayer = new PremiumPlayer();
+
+        usualPlayer.generateNumber();
+        premiumPlayer.generateNumber();
+
+//        if (usualPlayer.getNumber() < premiumPlayer.getNumber()) {
+//            return;
+//        }
+//        if (usualPlayer.getNumber() > premiumPlayer.getNumber()) {
+//            System.out.println("ya v shoke");
+//        }
+
+        System.out.println("Ход обычного игрока " + usualPlayer.getNumber());
+        System.out.println("Ход премиум игрока " + premiumPlayer.getNumber());
     }
 }
