6fb6f129e666ed1a42a3669ff8c7323155e1bf4a
Arthur <chupryna.2004@mail.ru>
2025-03-16 19:45:07 +0500

    add skip move & field name

--- src/Map.java
+++ src/Map.java
@@ -1,6 +1,25 @@
+import java.util.Random;
+
 class Map {
+
+    private String name;
     public int length;
-    public Map(int length) {
+
+    public Map(String name, int length) {
+        this.name = name;
         this.length = length;
+    }
+
+    public static boolean isSkipMove() {
+        Random random = new Random();
+        int num = random.nextInt(0, 11);
+        if (num > 9) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getName() {
+        return name;
     }
 }
