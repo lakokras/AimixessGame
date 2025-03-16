942c26cb37f96c3c3ca1374ade94e77657e6f492
Arthur <chupryna.2004@mail.ru>
2025-03-16 19:46:46 +0500

    add method calls

--- src/Program.java
+++ src/Program.java
@@ -2,11 +2,18 @@ public class Program {
 
     public static void main(String[] args) {
 
-        Player usualPlayer = new UsualPlayer();
-        Player premiumPlayer = new PremiumPlayer();
-        Map map = new Map();
+        Player usualPlayer = new UsualPlayer("Лузер");
+        Player simplePlayer = new UsualPlayer("ВАНЮХА");
+        Player premiumPlayer = new PremiumPlayer("Профик");
 
-        Game game = new Game([usualPlayer, premiumPlayer], map);
+        Player[] players = new Player[3];
+        players[0] = usualPlayer;
+        players[1] = simplePlayer;
+        players[2] = premiumPlayer;
+
+        Map map = new Map("YT Shorts", 1000);
+
+        Game game = new Game(players, map);
         game.start();
     }
 }
